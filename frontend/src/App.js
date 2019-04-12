686e12aef5e30881e94fbce0e32c0f8138090e13
Conor <conor.manning1996@gmail.com>
2019-04-11 21:16:37 -0400

    Temporarily showing Login and CreateAccount

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
 import CreateAccount from './CreateAccount'
+import Login from './Login'
+import {Container, Col} from 'react-bootstrap'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <CreateAccount/>
+      <div>
+        <Container>
+          <Col className="signin-block">
+            <CreateAccount/>
+          </Col>
+          <Col className="signin-block">
+            <Login/>
+          </Col>
+        </Container>
       </div>
     );
   }
