0928edff3af7221b99ac6b585c688a57f292c094
sheeryachavan <shreesh.chavan@gmail.com>
2019-05-05 08:40:07 -0400

    minor changes

--- frontend/src/CreateAccount.js
+++ frontend/src/CreateAccount.js
@@ -1,6 +1,6 @@
-import React, {Component} from 'react'
-import {Container, Row} from 'react-bootstrap'
-import {auth} from './firebase'
+import React, { Component } from 'react'
+import { Container, Row } from 'react-bootstrap'
+import { auth } from './firebase'
 
 class CreateAccount extends Component {
 
@@ -17,42 +17,42 @@ class CreateAccount extends Component {
     return (
       <div>
         <Container>
-        <form class="account-form">
-        <h1>Create Account</h1>
-          <Row>
-            <input 
-            type="text" 
-            class="text-input"
-            name="name"
-            onChange={this.changeHandler}
-            placeholder="Name:"
+          <form class="account-form">
+            <h1>Create Account</h1>
+            <Row>
+              <input
+                type="text"
+                class="text-input"
+                name="name"
+                onChange={this.changeHandler}
+                placeholder="Name:"
+              />
+            </Row>
+            <Row class="text-input">
+              <input
+                type="text"
+                class="text-input"
+                name="email"
+                onChange={this.changeHandler}
+                placeholder="Email address:"
+              />
+            </Row>
+            <Row class="text-input">
+              <input
+                type="password"
+                class="text-input"
+                name="password"
+                onChange={this.changeHandler}
+                placeholder="Password:"
+              />
+            </Row>
+            <input
+              type="button"
+              class="button"
+              value="Submit"
+              onClick={this.submit.bind(this)}
             />
-          </Row>
-          <Row class="text-input">
-            <input 
-            type="text" 
-            class="text-input"
-            name="email"
-            onChange={this.changeHandler}
-            placeholder="Email address:"
-            />
-          </Row>
-          <Row class="text-input">
-            <input 
-            type="password" 
-            class="text-input"
-            name="password" 
-            onChange={this.changeHandler}
-            placeholder="Password:"
-            />
-          </Row>
-          <input 
-          type="button" 
-          class="button"
-          value="Submit" 
-          onClick={this.submit.bind(this)}
-          />
-        </form>
+          </form>
         </Container>
       </div>
     )
@@ -67,14 +67,14 @@ class CreateAccount extends Component {
   }
 
   submit() {
-    if(this.state.email === "" || this.state.password === "") {
+    if (this.state.email === "" || this.state.password === "") {
       console.log("Empty field")
       return
     }
     auth.createUserWithEmailAndPassword(this.state.email, this.state.password)
       .then((res) => {
         const user = res.user
-        console.log(user.uid) 
+        console.log(user.uid)
       })
   }
 
