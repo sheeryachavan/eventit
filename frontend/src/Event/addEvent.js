290b1b25f24faccfc4232498c027b3afd49913a9
sheeryachavan <shreesh.chavan@gmail.com>
2019-05-05 08:34:46 -0400

    Event classes added

---
+++ frontend/src/Event/addEvent.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { Container, Row, Button } from 'react-bootstrap'
+class AddEvent extends Component{
+    constructor(props) {
+        super(props)
+        this.state = {
+            eventData: undefined
+        }
+    }
+    render(){
+        return null;
+    };
+}
+export default AddEvent;
