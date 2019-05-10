10ee322b721d75c81dc6aa1ddc972160d9d4c5a0
sheeryachavan <shreesh.chavan@gmail.com>
2019-05-10 16:06:57 -0400

    made responsive Navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,23 +7,49 @@ import Home from './Home';
 import EventContainer from './Events';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import { Switch } from 'react-router-dom';
-import Navbar from './Navigation/TopNavigation'
+import Navbar from './Navigation/TopNavigation';
+import SideBar from './Navigation/SideDrawer';
+import BackDrop from './Navigation/Backdrop';
 library.add(faStroopwafel)
 class App extends Component {
+  state = {
+    SideDrawerOpen: false
+  };
+  drawerToggleClickHandler = () => {
+    this.setState((prevState) => {
+      return { SideDrawerOpen: !prevState.SideDrawerOpen }
+    })
+  };
+  backDropClickHandler = () => {
+    this.setState({
+      SideDrawerOpen: false
+    }
+    )
+  }
   render() {
+
+    let backDrop;
+    if (this.state.SideDrawerOpen) {
+
+      backDrop = <BackDrop click={this.backDropClickHandler}/>;
+    }
     return (
       <div>
-        <Navbar />
-        <Router>
-          <div className="App">
-            <div className="App-body">
+        <div className="App">
+          <Navbar drawerClickHandler={this.drawerToggleClickHandler} />
+          <SideBar show = {this.state.SideDrawerOpen}/>
+          {backDrop}
+          <div className="App-body">
+            <Router>
               <Switch>
                 <Route path="/events" component={EventContainer} />
                 <Route path="/" component={Home} />
               </Switch>
-            </div>
+            </Router>
+
           </div>
-        </Router>
+        </div>
+
 
         <Footer></Footer>
       </div>
--- frontend/src/Navigation/TopNavigation.js
+++ frontend/src/Navigation/TopNavigation.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import './DrawerToggleButton'
 import './TopNavigation.css';
-const navbar  = props =>(
+import DrawerToggleButton from './DrawerToggleButton';
+import { checkPropTypes } from 'prop-types';
+const navbar = props => (
     <header className="clsToolbar">
         <nav className="clsToolbarNavigation">
-            <div></div>
+            <div className = "clsToolbarToggleButton">
+                <DrawerToggleButton click ={props.drawerClickHandler}/>
+            </div>
             <div className="clsToolbarLogo"><a href='/'>LOGO</a></div>
             <div className="spacer"></div>
             <div className="clsToolbarNavigationItems">
