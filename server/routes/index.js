e3d0cfa67f810c7cae3c6a7408a65af280ae88b0
Krutarth Trivedi <47464196+Krutarth29@users.noreply.github.com>
2019-05-12 20:14:52 -0400

    Update index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,14 +3,10 @@ const eventRoutes = require("./events_r");
 var cors = require('cors')
 
 const constructorMethod = app => {
-
     app.use("/eventit/user", userRoutes);
-
     app.use("/eventit/event", eventRoutes);
-
     app.use("*",cors(), (req, res) => {
         res.redirect("http://localhost:3001/eventit");
       });
 };
-
 module.exports = constructorMethod;  
