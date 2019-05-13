e2d1cdfefbe663e42cbeb36d495cf481a1c606be
Krutarth Trivedi <47464196+Krutarth29@users.noreply.github.com>
2019-05-12 20:04:18 -0400

    Error status added

--- server/routes/events_r.js
+++ server/routes/events_r.js
@@ -7,17 +7,17 @@ const userData = data.usersFirebase;
 
 router.post("/addevent",cors(),async(req, res) => {
 	try{
-    const upload = req.body;   
+    const upload = req.body;
     let result = await eventData.addEvent(upload.event_name, upload.event_type, upload.event_description, upload.event_location, upload.event_begin, upload.event_end, upload.event_owner, upload.event_ownerContact, upload.event_ownerPhone, upload.event_ownerContact,[],upload.event_count,upload.event_keyword);
-    
+
     if(!result)
       res.json({error:"Server is busy, please wait!"})
 
     let result2 = await userData.ownEventById(upload.event_owner,result.event_id)
-    res.json(result);
+    res.status(200).json(result);
     }
 	catch(e){
-		res.json({error:"Server is busy, please try latter!!"})
+		res.status(500).json({error:"Server is busy, please try latter!!"});
 	}
 });
 
@@ -25,12 +25,12 @@ router.get("/getevent/:id", cors(), async(req,res) =>{
   try{
     const result = await eventData.getEventById(req.params.id);
     if(result)
-        res.json(result);
+      res.status(200).json(result);
     else
       res.json({error:"Server is busy, please wait!"})
   }
   catch(e){
-    res.json({error:"Server is busy, please try latter!!"})
+		res.status(404).json({ message: "Event not found with this Id!" });
 }
 });
 function escapeRegex(text) {
@@ -40,10 +40,10 @@ router.get("/getAllEvents/:address", cors(), async(req,res)=>{
   try{
     const regex = new RegExp(escapeRegex(req.params.address), 'gi');
     var results = await eventData.getEventsByLocation(regex, req.params.address);
-    res.json(results);
+    res.status(200).json(results);
   }
   catch(e){
-    res.json({error:"Server is busy, please try latter!!"})
+		res.status(404).json({ message: "Event not found with this Location!" });
   }
 });
 
@@ -54,27 +54,27 @@ router.get("/getAllEventsByTag/:keyword", cors(), async(req,res)=>{
     res.status(200).json(results);
   }
   catch(e){
-    res.status(500).json({error:"Server is busy, please try latter!!"})
+    res.status(500).json({error:"Server is busy, please try latter!!"});
   }
 });
 
 router.get("/getAllEvents", cors(), async(req,res)=>{
   try{
     const results = await eventData.getAllEvents();
-    res.json(results);
+    res.status(200).json(results);
   }
   catch(e){
-    res.json({error:"Server is busy, please try latter!!"})
+		res.status(500).json({error:"Server is busy, please try latter!!"});
   }
 });
 
 router.put("/updateEvent/:id",cors(),async(req,res)=>{
   try{
-    const upload = req.body; 
-    
+    const upload = req.body;
+
     const oldOne = await eventData.getEventById(req.params.id);
     const updated = {};
-   
+
     updated.event_name = upload.event_name;
     updated.event_title = upload.event_title;
     updated.event_description = upload.event_description;
@@ -96,16 +96,16 @@ router.put("/updateEvent/:id",cors(),async(req,res)=>{
     //   temp[i] = oldOne.event_keyword[]
     // }
     const updatedResult = await eventData.updateEventById(req.params.id, updated);
-		res.json(updatedResult);
+		res.status(200).json(updatedResult);
   }
   catch(e){
-    res.json({error:"Server is busy, please try latter!!"})
+		res.status(500).json({error:"Server is busy, please try latter!!"});
   }
 });
 
 router.post("/joinEvent", cors(), async(req, res) => {
   try {
-    const upload = req.body; 
+    const upload = req.body;
     var info = await eventData.getEventById(upload.event_id);
     if(info.event_owner == upload.user_id){
       res.json({error:"You're the owner!"});
@@ -118,24 +118,22 @@ router.post("/joinEvent", cors(), async(req, res) => {
       }
     }
     let result2 = await userData.joinEventById(upload.user_id,upload.event_id);
-    
+
     info.event_joiners.push(upload.user_id);
     let result = await eventData.updateEventById(upload.event_id,info);
-    res.json(result);
-  } 
+    res.status(200).json(result);
+  }
   catch (e) {
-    res.json({error:"Server is busy, please try latter!!"});
-    // res.status(500).json({ error: e });
-    // return
+		res.status(500).json({error:"Server is busy, please try latter!!"});
   }
 });
 
 router.post("/cancelEvent", cors(), async(req, res) => {
   try {
-    const upload = req.body; 
+    const upload = req.body;
     var info = await eventData.getEventById(upload.event_id);
     let result2 = await userData.cancelEventById(upload.user_id,upload.event_id);
-    
+
     for(let i = 0; i < info.event_joiners.length; i++){
 			if (info.event_joiners[i] == upload.user_id)
 				var index = i;
@@ -143,12 +141,10 @@ router.post("/cancelEvent", cors(), async(req, res) => {
     info.event_joiners.splice(index,1);
     console.log(info);
     let result = await eventData.updateEventById(upload.event_id,info);
-    res.json(result);
-  } 
+    res.status(200).json(result);
+  }
   catch (e) {
-    res.json({error:"Server is busy, please try latter!!"});
-    // res.status(500).json({ error: e });
-    // return
+		res.status(500).json({error:"Server is busy, please try latter!!"});
   }
 });
 
@@ -160,11 +156,11 @@ router.get("/getOwenedEvents/:user_id", cors(), async(req,res)=>{
     if(user.owned.length == 0){
       res.status(200).json(results);
       return;
-    }    
+    }
     for(let i = 0; i < user.owned.length; i++){
       let result = {};
       result.event_id = user.owned[i];
-      
+
       let eventInfo = await eventData.getEventById(user.owned[i]);
       result.event_name = eventInfo.event_name;
       result.event_location = eventInfo.event_location;
@@ -190,11 +186,11 @@ router.get("/getJoinedEvents/:user_id", cors(), async(req,res)=>{
     if(user.joined.length == 0){
       res.status(200).json(results);
       return;
-    }    
+    }
     for(let i = 0; i < user.joined.length; i++){
       let result = {};
       result.event_id = user.owned[i];
-      
+
       let eventInfo = await eventData.getEventById(user.joined[i]);
       result.event_name = eventInfo.event_name;
       result.event_location = eventInfo.event_location;
@@ -215,6 +211,4 @@ router.get("/getJoinedEvents/:user_id", cors(), async(req,res)=>{
 router.get("/*", async(req, res) => {
   res.redirect("http://localhost:3001/eventit/");
 });
-
-
 module.exports = router;
