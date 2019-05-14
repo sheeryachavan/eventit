6e2dd3a36494c43ad3887d3667f5ccd945b17b7a
mfang2 <mfang013@gmail.com>
2019-05-14 16:26:05 -0400

    unblock redirect

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,8 +5,10 @@ var cors = require('cors')
 const constructorMethod = app => {
     app.use("/eventit/user", userRoutes);
     app.use("/eventit/event", eventRoutes);
+    /*
     app.use("*",cors(), (req, res) => {
         res.redirect("http://localhost:3001/eventit");
       });
+      */
 };
 module.exports = constructorMethod;  
